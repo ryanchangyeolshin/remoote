526229aeddbd0f0193d8f47a794f8aa010e13e2e
Ryan Shin <changyeolshin@gmail.com>
2023-07-26 13:45:02 +0800

    Make the UserProfileSavedJobs component scrollable.

--- src/actions/jobActions.ts
+++ src/actions/jobActions.ts
@@ -1,7 +1,6 @@
 import { JobActionTypes, type JobPostingType, type SaveJobAction } from '../Types/Jobs/types';
 
 export const saveJob = (jobDetails: JobPostingType): SaveJobAction => {
-  console.log(jobDetails);
   return {
     type: JobActionTypes.SAVE_JOB,
     payload: jobDetails
